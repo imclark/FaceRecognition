cd1efd52d6b6ff0dedd66d89e4736a8060576b8d
imclark <33160623+imclark@users.noreply.github.com>
2018-02-11 19:55:40 -0600

    Added debugging statements and comments

--- faceDetection.py
+++ faceDetection.py
@@ -7,32 +7,35 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
  #take in the image from the user's command
 file_name = sys.argv[1]
-logging.debug("Loading in image: " + file_name)
+logging.debug("-Loading in image: " + file_name)
 
 # create an instance of the HOG face deterctor
+logging.debug("-Initilizing the face detector")
 face_detector = dlib.get_frontal_face_detector()
-logging.debug("initilizing the face detector")
 
 # create a GUI window to view the image
+logging.debug("-Initilizing GUI image display")
 win = dlib.image_window()
 
 # format the image file into an array
+logging.debug("-Loading image file into an array")
 image = io.imread(file_name)
 
-# run the 
+# run the face detector with the image
+logging.debug("-Loading the image array into the face_detector")
 detected_faces = face_detector(image, 1)
 
-print("Found {} faces in the image {}".format(len(detected_faces), file_name))
-
-
+# load the image into the win and set the title
+logging.debug("-Loading the image array into the GUI image display")
 win.set_image(image)
 win.set_title("HOG Face Detector")
 
-
+# loop through the detected_faces and get an face_rect instance for each face regognized in the picture and place a border around the face
+logging.debug("-Overlaying borders onto the faces in the image")
 for i, face_rect in enumerate(detected_faces):
-
-	print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
-
 	win.add_overlay(face_rect)
-     
+
+print("\n -Found {} face(s) in the image {} \n".format(len(detected_faces), file_name))
+
+#function to make the gui image stay up untill you dont want it up anymore     
 dlib.hit_enter_to_continue()
