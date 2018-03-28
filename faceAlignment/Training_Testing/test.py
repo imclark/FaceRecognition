64b3be32eb295bcb4864a854e6e4620a1e53d33a
imclark <33160623+imclark@users.noreply.github.com>
2018-03-27 22:42:09 -0500

    Added predictor selection
    Also changed the testing xml to use

--- faceAlignment/Training_Testing/test.py
+++ faceAlignment/Training_Testing/test.py
@@ -7,15 +7,17 @@ from skimage import io
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 # make sure there is a path to img folder in the argument
-if len(sys.argv) != 2:
-    logging.error( "********** Please give a path to the face images direcotry for the argument! **********")
+if len(sys.argv) != 3:
+    logging.error( "********** Please give a path to the face images direcotry then the landmark_predictor dat for the argument! **********")
     exit()
 
 # load the img foldfer to a variable
 img_folder = sys.argv[1]
 
-testing_xml_path = os.path.join(img_folder, "training.xml")
+predictorPath = sys.argv[2]
 
-print("\n Testing Accuracy: {}".format( dlib.test_shape_predictor(testing_xml_path, "../Landmark_Data_Generator/data_generator.dat")))
+testing_xml_path = os.path.join(img_folder, "testing.xml")
+
+print("\n Testing Accuracy: {}".format( dlib.test_shape_predictor(testing_xml_path, predictorPath)))
 
 dlib.hit_enter_to_continue()
