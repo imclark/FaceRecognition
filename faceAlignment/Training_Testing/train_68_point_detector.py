f51bbeeda449966cea8b28f58ab2c94c74dc99b1
imclark <33160623+imclark@users.noreply.github.com>
2018-03-28 15:10:20 -0500

    Added naming functionality
    allow to specify what xml to train on
    allow user to name the predictor model

--- faceAlignment/Training_Testing/train_68_point_detector.py
+++ faceAlignment/Training_Testing/train_68_point_detector.py
@@ -28,16 +28,16 @@ training_options = dlib.shape_predictor_training_options()
 logging.debug("-Loaded in NN training options")
 
 # set the taining options, to know what each option does read the dlib documentation
-#training_options.oversampling_amount = 60
+training_options.oversampling_amount = 50
 
-training_options.nu = 0.04
+training_options.nu = 0.05
 
 #set the tree depth to 2
 training_options.tree_depth = 2
 
 training_options.num_trees_per_cascade_level = 2
 
-training_options.lambda_param = 0.1
+training_options.lambda_param = 0.2
 
 training_options.cascade_depth = 2
 
@@ -45,19 +45,27 @@ training_options.num_test_splits = 2
 
 training_options.feature_pool_size = 400
 
+training_options.feature_pool_region_padding = 0
+
+#training_options.num_threads = 2
+
 #setting the be_verbose setting to true so the training data will be printed out
 training_options.be_verbose = True
 
 # the training will take in an xml file with the imgs used for the training dataset
 # so we need to create that xml with the imgs in the given folder
-training_xml_path = os.path.join(img_folder, "all_img_data.xml")
+xml_name = raw_input("-----------xml file name (including .xml): ")
+
+training_xml_path = os.path.join(img_folder, xml_name)
 logging.debug("-Set the img xml")
 
 # now we create the predictor with the settings and imgs
-dlib.train_shape_predictor(training_xml_path, "trained_landmark_predictor.dat", training_options)
+model_name = raw_input("-----------model name (including .dat): ")
+
+dlib.train_shape_predictor(training_xml_path, model_name, training_options)
 logging.debug("-Trained the new predictor")
 
-print("\n Training Accuracy: {}".format(dlib.test_shape_predictor(training_xml_path, "trained_landmark_predictor.dat")))
+print("\n Training Accuracy: {}".format(dlib.test_shape_predictor(training_xml_path, model_name)))
 
 
 #now we wanna test the trained model with faces diffrent than the ones we used in training
