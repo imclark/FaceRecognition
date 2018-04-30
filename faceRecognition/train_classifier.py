7a68486798617d3e3358c3d31f9473abc854c0c3
imclark <clarkian15@gmail.com>
2018-04-30 14:58:37 -0700

    added formal documentation

--- faceRecognition/train_classifier.py
+++ faceRecognition/train_classifier.py
@@ -1,3 +1,26 @@
+############################################################################
+# 
+#   -This is a file to call to create and train a classifier
+#
+#   -Make sure you have the correct training image set-up
+#   ex:
+#       -training-images
+#       |__person_1
+#       |  |__person_1_image_1.jpg
+#       |  |__person_1_image_2.jpg
+#       |  .
+#       |__person_2
+#       |  |__person_2_image_1.jpg
+#       |  |__person_2_image_2.jpg
+#       |  .
+#
+#   -You'll be prompted to give paths to the specified files
+#
+#   -For naming of created classifier, if no name is given then a default
+#       name will be provided: 'trained_classifier.clf'
+#
+############################################################################
+
 import facePackages.FRP as FRP
 import os, os.path, pickle, math
 from sklearn import neighbors
@@ -10,7 +33,7 @@ print("----------- Welcome to the classifier trainer.")
 training_images = raw_input("----------- Please enter the path to the training image directory: ")
 
 # get the name to save the file to
-save_name = raw_input("----------- Save the classifier with a specific name with .clf at the end: ")
+save_name = raw_input("----------- Save the classifier with a specific name (include .clf at the end): ")
 
 print("----------- Training the classifier now!")
 
